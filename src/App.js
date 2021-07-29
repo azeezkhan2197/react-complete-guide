d05806d2d0e335286c27a3ca7fc12479026214cd
Azeez Khan <abdulazeez2197@gmail.com>
2021-07-29 23:21:03 +0530

    Passign method reference b/w components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
 
-  const app = props =>{
-    const[personsName, setPersonState] = useState({
-      persons :[
-        {name:"Max",age:28},
-        {name:"Stephen",age:29},
-        {name:"Manu",age:27},
-      ]
-  });
 
-  const switchNameHandler=()=>{
-    console.log('hello');
-    setPersonState({
+class App extends Component{
+  state = {
+    persons :[
+      {name:"Max",age:28},
+      {name:"Stephen",age:29},
+      {name:"Manu",age:27},
+    ]
+  }
+
+  switchNameHandler =(newName)=>{
+    this.setState({
       persons :[
-        {name:"Ronaldo",age:28},
-        {name:"Messi",age:29},
-        {name:"Manu",age:27},      
+        {name:newName,age:28},
+        {name:"Ronaldo",age:29},
+        {name:"Manu",age:27},
       ]
     })
   }
 
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <button onClick={switchNameHandler}>Change Name</button>
-      <Person name={personsName.persons[0].name} age={personsName.persons[0].age}/>
-      <Person name={personsName.persons[1].name} age={personsName.persons[1].age}>I love football</Person>
-      <Person name={personsName.persons[2].name} age={personsName.persons[2].age}/>
-    </div>
-  );
-}
+  render(){
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+        <button onClick={()=>this.switchNameHandler('Rooney')}>Change Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this,'Neymar')}>
+          I love football
+        </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
 
-export default app;
+}
+export default App;
