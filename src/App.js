0a3b36fd537a399dddd6aee1233823e261196c2c
Azeez Khan <abdulazeez2197@gmail.com>
2021-08-03 17:03:36 +0530

    adding two way binding

--- src/App.js
+++ src/App.js
@@ -31,14 +31,17 @@ class App extends Component{
     this.setState({persons:persons})
   }
 
-  nameChangeHandler =(event)=>{
-    this.setState({
-      persons :[
-        {name:"Messi",age:28},
-        {name:"Ronaldo",age:29},
-        {name:event.target.value,age:27},
-      ]
-    })
+  nameChangeHandler =(event,id)=>{
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({ persons : persons })
   }
 
   togglePersonHandler=()=>{
@@ -67,7 +70,9 @@ class App extends Component{
         <div>
           {this.state.persons.map((person,index) => {
              return <Person name={person.name} age={person.age} key={person.id} 
-             click={()=>this.deletePersonHandler(index)} />
+             click={()=>this.deletePersonHandler(index)}
+             changed={(event)=> this.nameChangeHandler (event,person.id)}
+             />
           })}
          </div>
       )
